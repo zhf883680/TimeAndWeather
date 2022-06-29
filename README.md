4edd5451dbae27ad345db4eb8132e17e7071bd87
zhoufeng <zhf883680@gmail.com>
2022-06-29 13:25:27 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,24 @@
+# 项目说明
+此项目用途为:**老旧手机**显示时间,天气使用  时间为12小时格式
+具体可显示内容:**时间,农历,星期,天气** 
+![图片](./show.jpg)  
+同时设定了**常亮**,天气每隔**2h**更新一次  
+目前定位是苏州,使用的是高德的API  
+## 备注
+无设置菜单,所有代码里完成:<font color=red>由于部分手机老旧以后触控不正常,所以只有一个显示页面</font>  
+## 环境
+安卓4.1  
+## 自行编译说明
+### 高德
+高德申请**key**,然后填入`AndroidManifest.xml`  
+且需要获取编译以后的sha1,具体看高德说明  
+```
+ <meta-data
+            android:name="com.amap.api.v2.apikey"
+            android:value="高德的key"/>
+```
+### 天气城市
+修改`WeatherSearch.java`  
+```
+private String city="苏州"; //调整成对应城市  
+```
---
+++ app/src/main/java/com/example/timeandweather/WeatherSearch.java
@@ -0,0 +1,72 @@
+package com.example.timeandweather;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.amap.api.services.core.AMapException;
+
+import com.amap.api.services.core.ServiceSettings;
+import com.amap.api.services.weather.LocalWeatherForecast;
+import com.amap.api.services.weather.LocalWeatherForecastResult;
+import com.amap.api.services.weather.LocalWeatherLive;
+import com.amap.api.services.weather.LocalWeatherLiveResult;
+
+import com.amap.api.services.weather.WeatherSearch.OnWeatherSearchListener;
+import com.amap.api.services.weather.WeatherSearchQuery;
+
+
+
+import java.util.List;
+public class WeatherSearch extends com.amap.api.services.weather.WeatherSearch implements com.amap.api.services.weather.WeatherSearch.OnWeatherSearchListener{
+    private Context context;
+    private String city="苏州";
+    private WeatherSearchQuery mquery;
+    private WeatherSearch mweathersearch;
+    private LocalWeatherLive weatherlive;
+    private LocalWeatherForecast weatherforecast;
+    public WeatherSearch(Context context) throws AMapException {
+        super(context);
+
+    }
+    /**
+     * 实时天气查询
+     */
+    public void searchliveweather() throws AMapException {
+        Log.i("thisApp","执行了查询");
+        mquery = new WeatherSearchQuery(city, WeatherSearchQuery.WEATHER_TYPE_LIVE);//检索参数为城市和天气类型，实时天气为1、天气预报为2
+        mweathersearch = new WeatherSearch(MainActivity.mContext);
+        mweathersearch.setOnWeatherSearchListener(this);
+        mweathersearch.setQuery(mquery);
+        mweathersearch.searchWeatherAsyn(); //异步搜索
+    }
+    /**
+     * 实时天气查询回调
+     */
+    @Override
+    public void onWeatherLiveSearched(LocalWeatherLiveResult weatherLiveResult, int rCode) {
+        if (rCode == AMapException.CODE_AMAP_SUCCESS) {
+            Log.i("thisApp","查询成功了");
+            if (weatherLiveResult != null && weatherLiveResult.getLiveResult() != null) {
+                weatherlive = weatherLiveResult.getLiveResult();
+                MainActivity.weatherInfo =city+":"+weatherlive.getWeather() +","+weatherlive.getTemperature()+"°," +weatherlive.getWindDirection() + "风" + weatherlive.getWindPower() + "级,"
+                        +"湿度:" + weatherlive.getHumidity() + "%";
+
+            } else {
+                MainActivity.weatherInfo="天气获取失败,2小时后重试";
+            }
+        } else {
+            MainActivity.weatherInfo="天气获取失败,2小时后重试";
+        }
+    }
+    @Override
+    public void onWeatherForecastSearched(
+            LocalWeatherForecastResult weatherForecastResult, int rCode) {
+        if (rCode == AMapException.CODE_AMAP_SUCCESS) {
+
+        } else {
+
+        }
+    }
+}
